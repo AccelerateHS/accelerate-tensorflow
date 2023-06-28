f274b3304d7d3db723d9ffe0fb076665fdc6731e
David van Balen <dpvanbalen@hotmail.com>
2023-06-28 12:15:42 +0200

    add test

---
+++ accelerate-tensorflow-lite/test/nofib/Data/Array/Accelerate/Test/NoFib/Prelude/Foreign.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeOperators       #-}
+-- |
+-- Module      : Data.Array.Accelerate.Test.NoFib.Prelude.Generate
+-- Copyright   : [2022] The Accelerate Team
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <trevor.mcdonell@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.Test.NoFib.Prelude.Foreign (
+
+  test_foreign
+
+) where
+
+import Data.Array.Accelerate.Test.NoFib.Base
+
+import Data.Array.Accelerate                                        as A
+import Data.Array.Accelerate.Interpreter                            as I
+import Data.Array.Accelerate.TensorFlow.Lite                        as TPU
+
+import Data.Array.Accelerate.Sugar.Shape
+
+import Hedgehog
+import qualified Hedgehog.Gen                                       as Gen
+import qualified Hedgehog.Range                                     as Range
+
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+import Prelude                                                      as P
+
+
+test_foreign :: TestTree
+test_foreign =
+  testGroup "foreign"
+    [ testDim dim1
+    , testDim dim2
+    , testDim dim3
+    ]
+    where
+      testDim :: forall sh. (Shape sh, Show sh, P.Eq sh)
+              => Gen (sh:.Int)
+              -> TestTree
+      testDim dim =
+        testGroup ("DIM" P.++ show (rank @sh))
+          [ testProperty "argmin" $ prop_min dim f32
+          -- , testProperty "argmax" $ prop_max dim i16
+          ]
+
+prop_min
+    :: (P.Eq sh, Show sh, Shape sh, Elt e, Show e, Similar e, A.Ord e)
+    => Gen (sh:.Int)
+    -> (WhichData -> Gen e)
+    -> Property
+prop_min dim e =
+  property $ do
+    sh  <- forAll dim
+    dat <- forAll (generate_sample_data sh e)
+    xs  <- forAll (array ForInput sh e)
+    let !f   = argMin
+        !ref = I.runN f
+        !tpu = TPU.compile f dat
+    --
+    TPU.execute tpu xs ~~~ ref xs
+
+
+generate_sample_data
+  :: (Shape sh, Elt e)
+  => (sh:.Int)
+  -> (WhichData -> Gen e)
+  -> Gen (RepresentativeData (Array (sh:.Int) e -> Array sh (Int32, e)))
+generate_sample_data (sh:.sz) e = do
+  xs <- Gen.list (Range.linear 10 16) (array ForSample (sh:.sz) e)
+  return [ x :-> Result sh | x <- xs ]
+
